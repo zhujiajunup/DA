eb7be8078092f730d461219e9f75f135acd838e8
jjzhu <jjzhu_zji@163.com>
2016-04-18 18:31:23 +0800

    matplotlib test

--- dataanalysis/matplotlibtest.py
+++ dataanalysis/matplotlibtest.py
@@ -1,2 +1,29 @@
 __author__ = 'jjzhu'
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sin_figure():
+    x = np.arange(-np.pi, np.pi, 0.01)
+    y = np.sin(x)
+    plt.plot(x, y, 'g')
+    plt.show()
+
+
+def plot():
+
+    x = [0, 1, 2, 3, 4, 5]
+    y = [0.1, 0.2, 0.2, 0.3, 0.2, 0.1]
+
+    plt.plot(x, y, '-.r')
+
+    plt.show()
+    plt.save("d:\\1.png")
+
+
+def main():
+    plot()
+
+if __name__ == '__main__':
+    main()
